09ac67bf7d192fd5179ea4d4f8536f331e03934f
Nuno Faria <nunofaria11@gmail.com>
2016-11-23 17:28:51 +0000

    SHA-1 safari

--- shared-secret/shared-secret-crypto.js
+++ shared-secret/shared-secret-crypto.js
@@ -266,7 +266,7 @@
       modulusLength: 2048,
       publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
       hash: {
-        name: 'SHA-1'
+        name: 'SHA-1' // Safari support
       }
     };
 
